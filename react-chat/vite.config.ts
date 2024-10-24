d9ba6d6478a08a882e6c6a88762c1af2e92a9db0
Belocherkovec <belocherkovec@gmail.com>
2024-10-25 02:05:40 +0300

    hw-5: фикс деплоя на gh-pages и сборки с base

--- react-chat/vite.config.ts
+++ react-chat/vite.config.ts
@@ -3,22 +3,31 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/2024-2-VK-EDU-Frontend-N-Kobyakov',
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
+export default defineConfig(({ command }) => {
+  let base = '/';
+
+  if (command === 'build') {
+    base = '/2024-2-VK-EDU-Frontend-N-Kobyakov';
+  }
+
+  return {
+    base,
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
       }
-    }
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve('src')
-    }
-  },
-  server: {
-    open: true
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    server: {
+      open: true
+    },
+    logLevel: 'info'
   }
 });
