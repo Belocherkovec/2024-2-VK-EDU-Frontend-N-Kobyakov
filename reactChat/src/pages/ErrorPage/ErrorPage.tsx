c84e2b92e17262af95d02323cb2821b1a3c413ec
Belocherkovec <belocherkovec@gmail.com>
2024-10-31 23:59:39 +0300

    hw-6: Добавил редиррект на главную страницу после очистки локального хранилища

--- reactChat/src/pages/ErrorPage/ErrorPage.tsx
+++ reactChat/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,22 +1,13 @@
+import { useErrorPage } from '@/pages/ErrorPage/hooks/useErrorPage.ts';
 import { TEXTS } from '@/shared/consts/texts';
 import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
 import cn from 'classnames';
-import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import styles from './errorPage.module.scss';
 
 export const ErrorPage: React.FC = () => {
-  const error = useRouteError();
-
-  let errorMessage: string;
-
-  if (isRouteErrorResponse(error)) {
-    errorMessage = `Error ${error.status}: ${error.statusText}`;
-  } else if (error instanceof Error) {
-    errorMessage = error.message;
-  } else {
-    errorMessage = 'Unknown error occurred';
-  }
+  const { errorMessage, handleClearButtonClick } = useErrorPage();
 
   return (
     <section className={styles.error}>
@@ -26,12 +17,7 @@ export const ErrorPage: React.FC = () => {
         <i>{errorMessage}</i>
       </p>
       <p>{TEXTS.pages.errorPage.callToAction}</p>
-      <button
-        className={styles.error__button}
-        onClick={() => {
-          localStorage.clear();
-        }}
-      >
+      <button className={styles.error__button} onClick={handleClearButtonClick}>
         {TEXTS.buttons.clearStorage}
       </button>
       <Link className={cn(styles.error__button, styles._noAccent)} to={'/'}>
---
+++ reactChat/src/pages/ErrorPage/hooks/useErrorPage.ts
@@ -0,0 +1,27 @@
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError
+} from 'react-router-dom';
+
+export const useErrorPage = () => {
+  const navigate = useNavigate();
+  const error = useRouteError();
+
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = `Error ${error.status}: ${error.statusText}`;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else {
+    errorMessage = 'Unknown error occurred';
+  }
+
+  const handleClearButtonClick = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
+  return { errorMessage, handleClearButtonClick };
+};
