affedf9f83904e2b618b13208802077acfcdfd17
Belocherkovec <belocherkovec@gmail.com>
2024-12-05 23:06:35 +0300

    hw-11: Реализована ленивая загрузка изображений в проекте и проведены оптимизации по заданию

--- reactChat/src/entities/Message/Message.tsx
+++ reactChat/src/entities/Message/Message.tsx
@@ -1,4 +1,4 @@
-import { Gallery, StatusMark, TMessageStatuses } from '@/shared';
+import { Gallery, LazyImage, StatusMark, TMessageStatuses } from '@/shared';
 import cn from 'classnames';
 import { forwardRef, useRef, useState } from 'react';
 
@@ -23,9 +23,12 @@ export const Message = forwardRef<
     const [isGalleryVisible, setIsGalleryVisible] = useState(false);
 
     const handleGalleryVisible = () => setIsGalleryVisible(false);
-    const handleImageClick = (id: number) => {
-      imageClickId.current = id;
-      setIsGalleryVisible(true);
+    const handleImageClick = (event: React.MouseEvent<HTMLFieldSetElement>) => {
+      const index = event.currentTarget.getAttribute('data-id');
+      if (index) {
+        imageClickId.current = +index;
+        setIsGalleryVisible(true);
+      }
     };
 
     return (
@@ -46,12 +49,13 @@ export const Message = forwardRef<
         <div className={styles.message__imageWrapper}>
           {files &&
             files.map(({ item }, id) => (
-              <img
-                src={item}
-                key={item}
-                alt="Пользовательская картинка"
+              <LazyImage
                 className={styles.message__image}
-                onClick={() => handleImageClick(id)}
+                src={item}
+                alt="Пользовательское изображение"
+                key={item}
+                onClick={handleImageClick}
+                data-id={id}
               />
             ))}
         </div>
