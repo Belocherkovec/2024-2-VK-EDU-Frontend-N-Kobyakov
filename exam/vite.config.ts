773a1c6e87813139c4d27bb4b3ab4a2135bb03ce
Belocherkovec <belocherkovec@gmail.com>
2024-12-23 21:42:52 +0300

    Фикс ошибок при deploy.

--- exam/vite.config.ts
+++ exam/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  let base = '/';
+
+  if (command === 'build') {
+    base = '/2024-2-VK-EDU-Frontend-N-Kobyakov';
+  }
+
+
+  return {
+    base,
+    plugins: [react()],
+  };
+});
